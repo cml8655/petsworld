8d5c26eedd5ef2ad96bb335d00295cb1a31809d4
menglin.chen <menglin.chen@team-lab.cn>
2014-09-12 12:15:42 +0800

    迅速旋转

--- PullFlushListView/gen/com/cml/pullflushlistview/R.java
+++ PullFlushListView/gen/com/cml/pullflushlistview/R.java
@@ -28,8 +28,9 @@ public final class R {
         public static final int arrow=0x7f020001;
         public static final int default_ptr_rotate=0x7f020002;
         public static final int ic_launcher=0x7f020003;
-        public static final int pull_flesh_pro=0x7f020004;
-        public static final int pull_pro_anim=0x7f020005;
+        public static final int loading2=0x7f020004;
+        public static final int pull_flesh_pro=0x7f020005;
+        public static final int pull_pro_anim=0x7f020006;
     }
     public static final class id {
         public static final int action_settings=0x7f09000a;
